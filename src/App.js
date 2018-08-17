f9e2c70496298c3891698a27f06fdc9dae6871a4
Pete Whittle <peterwhittle75@gmail.com>
2018-08-17 13:45:43 +0100

    Got rid of files relating to previous project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import GameContainer from './containers/GameContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <GameContainer/>
+      <p>Testing</p>
         );
   }
 }
