88f2b62d6511b80232b1393bf8f3b86141c055ee
Pete Whittle <peterwhittle75@gmail.com>
2018-08-18 15:15:40 +0100

    Working on delete specified route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, func
   const db = client.db("stockdb");
   console.log("Connected to db");
 
-//IEX API CALLS
+  //IEX API CALLS
 
   app.get('/', function(req, res){
     fetch('https://api.iextrading.com/1.0/stock/market/collection/sector?collectionName=Technology')
@@ -36,28 +36,28 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, func
   });
 
 
-//DATABASE CALLS
+  //DATABASE CALLS
 
   // post a new line of stock in the user's portfolio
 
   app.post('/stocks', function(req, res) {
-      console.log("stocks called");
-      const stocksCollection = db.collection('stocks');
-      const stockToSave = req.body;
-      stocksCollection.save(stockToSave, function (error, result) {
-        if(error){
-          console.log(error);
-          res.status(500);
-          res.send();
-        }
-        console.log('saved to database');
-        res.status(201);
-        res.json(result.ops[0]); // result object > operations property
+    const stocksCollection = db.collection('stocks');
+    const stockToSave = req.body;
+    stocksCollection.save(stockToSave, function (error, result) {
+      if(error){
+        console.log(error);
+        res.status(500);
+        res.send();
+      }
+      console.log('saved to database');
+      res.status(201);
+      res.json(result.ops[0]); // result object > operations property
     });
   });
 
   // get all stocks
   app.get('/stocks', function(req, res) {
+    console.log("stocks called");
     const stocksCollection = db.collection('stocks');
     stocksCollection.find().toArray(function(error, allStocks) {
       if(error){
@@ -71,6 +71,7 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, func
 
   //update specified stock
   app.put('/stocks/:id', function(req, res){
+    console.log("stock updated");
     const stocksCollection = db.collection('stocks');
     const stockID = ObjectID(req.params.id);
     const filterObject = {_id: stockID};
@@ -89,9 +90,9 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, func
 
   //delete all stocks
   app.delete('/stocks', function(req, res){
+    console.log("All stocks deleted");
     const filterObject = {};
     const stocksCollection = db.collection('stocks');
-
     stocksCollection.deleteMany(filterObject, function(err, result){
       if(err){
         res.status(500);
@@ -102,8 +103,24 @@ MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, func
     })
   })
 
-  app.listen(3001, function(){
-    console.log("App running");
+  //delete specified stock
+  app.delete('/stocks/:id', function (req, res){
+    const stocksCollection = db.collection('stocks');
+    const objectID = ObjectID(req.params.id);
+    const filterObject = {_id: objectID};
+
+    stocksCollection.deleteOne(filterObject, function(err, result){
+      if(err){
+        res.status(500);
+        res.send();
+      }
+      res.status(200);
+      res.json(result);
+    });
   });
+
+app.listen(3001, function(){
+  console.log("App running");
+});
 
 });
