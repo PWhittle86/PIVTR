e27d12828750967a087b51315f5a768f0a0b7a37
Campbell Miller <campbellmiller4e@gmail.com>
2018-08-21 23:07:09 +0100

    edits intro

--- src/components/InfoCompany.js
+++ src/components/InfoCompany.js
@@ -11,61 +11,20 @@ class InfoCompany extends React.Component{
   }
 
   componentDidUpdate(prevProps, prevState){
-<<<<<<< HEAD
-<<<<<<< HEAD
-    console.log(prevProps.selectedStock.epic, this.props.selectedStock.epic);
-    // debugger;
-    if(prevProps.selectedStock.epic !== this.props.selectedStock.epic){
-      const epic = this.props.selectedStock.epic
-
-=======
-
-    if(prevProps.selectedStock.epic !== this.props.selectedStock.epic){
-      const epic = this.props.selectedStock.epic
->>>>>>> develop
-      fetch(`https://api.iextrading.com/1.0/stock/${epic}/news`)
-      .then(response => response.json())
-      .then(companyNews => this.setState({companyNews: companyNews}))
-    }
-
-<<<<<<< HEAD
-
-
-    // fetch(`https://api.iextrading.com/1.0/stock/${epic}/company`)
-    // .then(response => response.json())
-    // .then(companyInfo => this.setState({companyInfo: companyInfo}))
-
-
-=======
-    if(prevProps.selectedStock.epic !== this.props.selectedStock.epic){
-      const epic = this.props.selectedStock.epic
-=======
 
 
     if(prevProps.selectedStock.epic !== this.props.selectedStock.epic){
       const epic = this.props.selectedStock.epic
->>>>>>> ca21444165909a95eeb70a0190ec27a518161681
 
       fetch(`https://api.iextrading.com/1.0/stock/${epic}/company`)
       .then(response => response.json())
       .then(companyInfo => this.setState({companyInfo: companyInfo}))
     }
-<<<<<<< HEAD
->>>>>>> develop
-=======
->>>>>>> ca21444165909a95eeb70a0190ec27a518161681
   }
 
   render(){
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return(
-      <React.Fragment>
-=======
-=======
 
->>>>>>> ca21444165909a95eeb70a0190ec27a518161681
     const newsArticles = this.state.companyNews.map((newsItem, index) => {
       return(
         <section>
@@ -78,10 +37,6 @@ class InfoCompany extends React.Component{
 
     return(
       <section>
-<<<<<<< HEAD
->>>>>>> develop
-=======
->>>>>>> ca21444165909a95eeb70a0190ec27a518161681
       <ul className="companyInfo">
         <li>{this.state.companyInfo.companyName}</li>
         <li>Industry: {this.state.companyInfo.industry}</li>
@@ -89,19 +44,10 @@ class InfoCompany extends React.Component{
         <li>Exchange: {this.state.companyInfo.exchange}</li>
         <li>{this.state.companyInfo.website}</li>
       </ul>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      </React.Fragment>
-=======
-      {newsArticles}
-    </section>
->>>>>>> develop
-=======
 
       {newsArticles}
     </section>
 
->>>>>>> ca21444165909a95eeb70a0190ec27a518161681
     )
   }
 }
